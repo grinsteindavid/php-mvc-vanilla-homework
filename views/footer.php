7e0085020c8dca18c66fcd4dfed46190ad87a074
david miranda <davidmirandagrinstein@gmail.com>
2017-02-11 09:28:25 -0400

    footer and pages template modules updated

---
+++ views/footer.php
@@ -0,0 +1,26 @@
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='https://www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+    <!-- build:js scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="../../public/scripts/vendor.js"></script>
+    <script src="../../public/scripts/vendor/modernizr.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/plugins.js -->
+    <script src="../../public/scripts/plugins.js"></script>
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/main.js -->
+    <script src="../../public/scripts/main.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
